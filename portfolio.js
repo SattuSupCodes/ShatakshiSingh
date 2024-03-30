703690e28e2d67cace5e1c196535d79098c1d145
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-03-30 21:37:39 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,13 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontent = document.getElementsByClassName("tab-content");
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablinks.classList.remove("active-link");
 
+    }
+    for(tabcontent of tabcontent){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
