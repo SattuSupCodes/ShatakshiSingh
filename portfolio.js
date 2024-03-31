944bdad2a71bd3ae22ea93f64618f767c89f1a81
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-03-31 22:08:52 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,25 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+    
+}
+
+  
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbysF6OXoqb7ac_uxVvijrffga83Da5pLc7QXXOxjB0I7e3JJQfa-P3eHOVB5KrKBJ5P/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
+  
